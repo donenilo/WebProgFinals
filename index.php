806252220816e19808a93f976c3302c6b01467fb
Aishifishy <115207646+Aishifishy@users.noreply.github.com>
2024-11-04 02:06:32 +0800

    2.0

--- index.php
+++ index.php
@@ -33,7 +33,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
     <link rel="stylesheet" href="css/styles.css">
     <script src="js/scripts.js"></script>
 </head>
