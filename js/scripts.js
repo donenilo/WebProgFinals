4a63b77e0518756a4ba867c8b2fdceaf5b930874
Aishifishy <115207646+Aishifishy@users.noreply.github.com>
2024-11-05 12:21:10 +0800

    Added dynamic data to balance chart

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,46 @@
-const xValues = ["Remaining Balance", "Total Spent"];
-const yValues = [35.2,56.8];
-const barColors = [
-  "#b91d47",
-  "#00aba9"
-];
+// // Get PHP variables Will transition to inline script instead of external
+// console.log("<?php echo $Tremaining; ?>");
+// console.log("<?php echo $TExpense; ?>");
+// document.addEventListener('DOMContentLoaded', function () {
+// const remainingBalance = parseFloat("<?php echo $Tremaining; ?>");
+// const totalSpent = parseFloat("<?php echo $TExpense; ?>");  
 
-new Chart("myChart", {
-    type: "pie",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: "Balance Chart"
-      }
-    }
-  });
+// new Chart("myChart", {
+//     type: "pie",
+//     data: {
+//       labels: ["Total Spent", "Remaining Balance"],
+//       datasets: [{
+//         backgroundColor: ["#b91d47", "#00FF00"],
+//         data: [totalSpent, remainingBalance]
+//       }]
+//     },
+//     options: {
+//       title: {
+//         display: true,
+//         text: "Balance Chart"
+//       }
+//     }
+//   });
+// });
 
-const aValues = ["", "", "", "", "",""];
-const bValues = [19, 38, 57, 38, 57, 57];
-const barColors1 = ["black", "black","black","black","black", "black"];
+// const aValues = ["", "", "", "", "",""];
+// const bValues = [19, 38, 57, 38, 57, 57];
+// const barColors1 = ["black", "black","black","black","black", "black"];
 
-new Chart("yourChart", {
-type: "bar",
-data: {
-    labels: aValues,
-    datasets: [{
-    backgroundColor: barColors1,
-    data: bValues
-    }]
-},
-options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "Chart data will be shown here"
-    }
-}
-});
+// new Chart("yourChart", {
+// type: "bar",
+// data: {
+//     labels: aValues,
+//     datasets: [{
+//     backgroundColor: barColors1,
+//     data: bValues
+//     }]
+// },
+// options: {
+//     legend: {display: false},
+//     title: {
+//       display: true,
+//       text: "Chart data will be shown here"
+//     }
+// }
+// });
